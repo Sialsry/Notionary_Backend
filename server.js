e4efde391fed0fa25ec30bfb495ae11fe972b797
Sialsry <alsryendtm@naver.com>
2025-05-15 11:45:05 +0900

    d

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({origin : '*'}))
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', (req,res) => [
-    res.json('홈페이지')
-])
-
+app.use("/", (req, res) => [res.json("홈페이지")]);
 
 app.listen(4000, (req, res) => {
-    console.log('server on~')
-})
+  console.log("server on~");
+});
+
+// wntjr
