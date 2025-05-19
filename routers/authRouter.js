58af1aa61e7b02f93ea7d452a68239edd40d27fd
Sialsry <alsryendtm@naver.com>
2025-05-19 17:23:28 +0900

    카카오 로그인 구현만 하고 마이페이지 들어갈게요

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const loginController = require("../controllers/loginController");
+const { auth } = require("../middlewares/authMiddleware");
 
-// Route for checking if a user ID is already taken
+// 기존 인증 관련 라우트
 router.post("/check-id", authController.checkIdDuplicate);
-
-// Route for registering a new user
 router.post("/register", authController.register);
+
+// 로그인 관련 라우트 추가
+router.post("/login", loginController.login);
+router.get("/verify", auth, loginController.verifyToken);
 
 module.exports = router;
