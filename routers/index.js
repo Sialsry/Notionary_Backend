37cbe644c6aa8aba0bf8011cb0d3a8af0463c509
blockSuho <akakak695@naver.com>
2025-05-19 18:01:36 +0900

    feat 20250519 susu 메인 페이지 전체 카테고리 기능 추가

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ const postRouter = require("./postRouter");
 const signRouter = require("./signRouter");
 const mypageRouter = require("./mypageRouter");
 const myprojectRouter = require("./myprojectRouter");
-const teamprojectRouter = require("./teamporjectRouter");
+const teamprojectRouter = require("./teamprojectRouter");
 const loginCheck = require("./middleware");
 
 module.exports = {
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,6 +1,13 @@
+const { getAllPost } = require("../controllers/post/post.controller");
+
 const router = require("express").Router();
 // const {musicController} =require('../controllers');
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    const data = await getAllPost();
+    res.json(data);
+});
 
 module.exports = router;
+
+
