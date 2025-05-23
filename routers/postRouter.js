dea37287f2d3d74932a6d0a9737045095200df3c
blockSuho <akakak695@naver.com>
2025-05-23 18:20:39 +0900

    feat 20250523 multer 파일 추가

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,6 +1,12 @@
+const { CreatePost } = require("../controllers/post/post.controller");
+const { upload } = require("../middlewares/multer");
+
 const router = require("express").Router();
 // const {musicController} =require('../controllers');
 
-router.get("/", async (req, res) => {});
+router.post("/post", upload.array(), async (req, res) => {
+    const data = await CreatePost(post_id, uid, category_id, title, content, imgPaths, videoPaths)
+    res.json(data)
+});
 
 module.exports = router;
