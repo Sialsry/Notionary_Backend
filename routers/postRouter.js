f66d36c9599c17f83340e9a6671018a6ec53bc14
Sialsry <alsryendtm@naver.com>
2025-06-01 16:25:07 +0900

    adsf

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,66 +1,101 @@
-const { CreatePost, getUserWorkspaces, UpdatePost, getPostById} = require("../controllers/post/post.controller");
+const {
+  CreatePost,
+  getUserWorkspaces,
+  UpdatePost,
+  getPostById,
+} = require("../controllers/post/post.controller");
 const { upload } = require("../middlewares/multer");
 
 const router = require("express").Router();
 // const {musicController} =require('../controllers');
-   // console.log("서버에 전달될 목록",post_id, uid, category_id, title, content )
-    // console.log("req.body:", req.body);
-    // console.log("제목", req.body.title)
-    // console.log("내용", req.body.content)
+// console.log("서버에 전달될 목록",post_id, uid, category_id, title, content )
+// console.log("req.body:", req.body);
+// console.log("제목", req.body.title)
+// console.log("내용", req.body.content)
 
-router.post("/", upload.array('media', 5), async (req, res) => {
-    const { post_id, uid, category_id, fk_workspace_id, title, content} = req.body;
-    console.log("req.files:", req.files);
-    const imgPaths = req.files
-    .filter(file => file.mimetype.startsWith('image/'))
-    .map(file => {
-        const relativePath = file.path.replace(/\\/g, '/').replace(/^public\//, '');
-        return `http://localhost:4000/${relativePath}`;
+router.post("/", upload.array("media", 5), async (req, res) => {
+  const { post_id, uid, category_id, fk_workspace_id, title, content } =
+    req.body;
+  console.log("req.files:", req.files);
+  const imgPaths = req.files
+    .filter((file) => file.mimetype.startsWith("image/"))
+    .map((file) => {
+      const relativePath = file.path
+        .replace(/\\/g, "/")
+        .replace(/^public\//, "");
+      return `http://localhost:4000/${relativePath}`;
     });
 
-    const videoPaths = req.files
-    .filter(file => file.mimetype.startsWith('video/'))
-    .map(file => {
-        const relativePath = file.path.replace(/\\/g, '/').replace(/^public\//, '');
-        return `http://localhost:4000/${relativePath}`;
+  const videoPaths = req.files
+    .filter((file) => file.mimetype.startsWith("video/"))
+    .map((file) => {
+      const relativePath = file.path
+        .replace(/\\/g, "/")
+        .replace(/^public\//, "");
+      return `http://localhost:4000/${relativePath}`;
     });
 
-    const data = await CreatePost({ post_id, uid, category_id, fk_workspace_id, title, content, imgPaths, videoPaths});
+  const data = await CreatePost({
+    post_id,
+    uid,
+    category_id,
+    fk_workspace_id,
+    title,
+    content,
+    imgPaths,
+    videoPaths,
+  });
 
-    res.json(data);
+  res.json(data);
 });
 
 router.post("/getWorkspace", async (req, res) => {
-    const { uid } = req.body;
-    console.log("하 진짜", uid)
-    const data = await getUserWorkspaces(uid)
-    res.json(data);
-})
+  const { uid } = req.body;
+  console.log("하 진짜", uid);
+  const data = await getUserWorkspaces(uid);
+  res.json(data);
+});
 
 router.put("/edit/:post_id", upload.array("media", 5), async (req, res) => {
   const { post_id } = req.params;
   const { uid, category_id, fk_workspace_id, title, content } = req.body;
   console.log("▶▶▶ 백엔드 디버그: req.body.uid 값:", req.body.uid);
 
- console.log("req.files:", req.files);
   const imgPaths = req.files
     .filter((file) => file.mimetype.startsWith("image/"))
-    .map((file) => `http://localhost:4000/${file.path.replace(/^public\//, "")}`);
+    .map((file) => {
+      const relativePath = file.path
+        .replace(/\\/g, "/")
+        .replace(/^public\//, "");
+      return `http://localhost:4000/${relativePath}`;
+    });
 
   const videoPaths = req.files
     .filter((file) => file.mimetype.startsWith("video/"))
-    .map((file) => `http://localhost:4000/${file.path.replace(/^public\//, "")}`);
-
-    const data = await UpdatePost({ post_id, uid, category_id, fk_workspace_id, title, content, imgPaths,videoPaths,});
-    res.json(data);
+    .map((file) => {
+      const relativePath = file.path
+        .replace(/\\/g, "/")
+        .replace(/^public\//, "");
+      return `http://localhost:4000/${relativePath}`;
+    });
+  const data = await UpdatePost({
+    post_id,
+    uid,
+    category_id,
+    fk_workspace_id,
+    title,
+    content,
+    imgPaths,
+    videoPaths,
+  });
+  res.json(data);
 });
 
 router.get("/:post_id", async (req, res) => {
   const { post_id } = req.params;
-//   console.log("해당 게시글 ID",post_id)
+  //   console.log("해당 게시글 ID",post_id)
   const data = await getPostById(post_id);
   res.json(data);
 });
-
 
 module.exports = router;
