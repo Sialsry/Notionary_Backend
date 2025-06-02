2c7a2ea17ac5e8d61823938847ebc6c91cfa4ba9
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-06-02 12:55:16 +0900

    final version

--- script/category_insert.js
+++ script/category_insert.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2/promise");
 const dbConfig = {
   host: "localhost",
   user: "root",
-  password: "suho4029",
-  database: "project",
+  password: "bing123",
+  database: "Reactproject",
 };
 
 // 카테고리 데이터
